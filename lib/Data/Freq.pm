f80e99cad2d67951eb2b950c8af02d9dbcca1e88
Mahiro Ando <ma514y@gmail.com>
2014-07-20 14:06:30 +0900

    add github links

--- lib/Data/Freq.pm
+++ lib/Data/Freq.pm
@@ -765,7 +765,11 @@ You can also look for information at:
 
 =over 4
 
-=item * RT: CPAN's request tracker (report bugs here)
+=item * GitHub repository (report bugs here)
+
+L<https://github.com/mahiro/perl-Data-Freq>
+
+=item * RT: CPAN's request tracker (report bugs here, alternatively)
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Data-Freq>
 
